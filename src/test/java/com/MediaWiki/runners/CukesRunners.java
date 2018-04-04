9a056dbbdd2892aba8b22f982aa5baffaab1658e
dmytroyurchyk <dimayrchuk@gmail.com>
2018-04-04 16:19:37 -0400

    fixed mistakes

--- src/test/java/com/MediaWiki/runners/CukesRunners.java
+++ src/test/java/com/MediaWiki/runners/CukesRunners.java
@@ -7,12 +7,7 @@ public class CukesRunners {
 	@CucumberOptions(plugin = { "pretty",
 			"html:target/cucumber-report"
 			},
-			
-<<<<<<< HEAD
-			tags = "@uplad_file",
-=======
 			tags = "@printable",
->>>>>>> branch 'master' of https://github.com/danazhanbekova/MediaWikiCrazyHackers.git
 			features = "src/test/resources/com/MediaWiki/features/",
 			glue = "com/MediaWiki/step_definitions/",
 			dryRun = false)
