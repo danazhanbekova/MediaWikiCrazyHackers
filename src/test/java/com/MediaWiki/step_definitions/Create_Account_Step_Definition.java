96609a14d9ee5091057ac2e5613e2f554f72fa5c
danazhanbekova <danazhanbekova@gmail.com>
2018-04-05 13:49:50 -0400

    finished create account steps_defs

--- src/test/java/com/MediaWiki/step_definitions/Create_Account_Step_Definition.java
+++ src/test/java/com/MediaWiki/step_definitions/Create_Account_Step_Definition.java
@@ -1,11 +1,11 @@
 package com.MediaWiki.step_definitions;
 
-
 import com.MediaWiki.pages.HomePage;
 import com.MediaWiki.utilities.ConfigurationReader;
 import com.MediaWiki.utilities.Driver;
 import com.MediaWiki.utilities.NumbersUtil;
 
+import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
@@ -13,65 +13,38 @@ import cucumber.api.java.en.When;
 public class Create_Account_Step_Definition {
 
 	HomePage homePage = new HomePage();
-<<<<<<< HEAD
-	//CreateAccountPage account = new CreateAccountPage();
 	NumbersUtil number = new NumbersUtil();
-	
-	
-=======
-	CreateAccountPage account = new CreateAccountPage();
 
->>>>>>> branch 'master' of https://github.com/danazhanbekova/MediaWikiCrazyHackers.git
 	@Given("^I open a MediaWiki page$")
 	public void i_open_a_MediaWiki_page() {
 		System.out.println("Loged into MediaWiki");
 		Driver.getDriver().get(ConfigurationReader.getProperty("url"));
-<<<<<<< HEAD
-=======
-
->>>>>>> branch 'master' of https://github.com/danazhanbekova/MediaWikiCrazyHackers.git
 	}
 
 	@When("^I click on Create Account link$")
 	public void i_click_on_Create_Account_link() {
-<<<<<<< HEAD
 		homePage.createAccount.click();
-=======
-		account.createAccount.click();
-		assertEquals(
-				"http://34.223.219.142:1022/mediawiki/index.php?title=Special:CreateAccount&returnto=Special%3AUpload",
-				Driver.getDriver().getCurrentUrl());
-
->>>>>>> branch 'master' of https://github.com/danazhanbekova/MediaWikiCrazyHackers.git
 	}
 
-	@When("^I enter registration credentials in the \"([^\"]*)\", \"([^\"]*)\", \"([^\"]*)\"$")
-<<<<<<< HEAD
-	public void i_enter_registration_credentials_in_the() {
-	    
-=======
-	public void i_enter_registration_credentials_in_the(String arg1, String arg2, String arg3) {
-
->>>>>>> branch 'master' of https://github.com/danazhanbekova/MediaWikiCrazyHackers.git
+	@And("^I enter registration credentials in the \"([^\"]*)\", \"([^\"]*)\", \"([^\"]*)\"$")
+	public void i_enter_registration_credentials_in_the(String username, String password, String confirm_password) {
+		homePage.userName.sendKeys(username);
+		homePage.password.sendKeys(password);
+		homePage.confirmPassword.sendKeys(confirm_password);
+		homePage.createYourAccount.click();
 	}
 
 	@Then("^I should get failure message \"([^\"]*)\"$")
-<<<<<<< HEAD
-	public void i_should_get_failure_message() {
-	    
-=======
-	public void i_should_get_failure_message(String arg1) {
-
->>>>>>> branch 'master' of https://github.com/danazhanbekova/MediaWikiCrazyHackers.git
+	public void i_should_get_failure_message(String message) {
+		System.out.println(message);
 	}
-<<<<<<< HEAD
-	
+
+
 	// 2nd
-	
-	
+
 	@When("^I create a new account with random username$")
 	public void i_create_a_new_account_with_random_username() {
-		
+
 		homePage.createAccount.click();
 		homePage.userName.sendKeys((homePage.getRandomLowercaseLetters(10)));
 		homePage.password.sendKeys("abc123");
@@ -83,7 +56,5 @@ public class Create_Account_Step_Definition {
 	public void i_should_be_able_to_create_a_new_account() {
 		homePage.newAccount.isDisplayed();
 	}
+
 }
-=======
-}
->>>>>>> branch 'master' of https://github.com/danazhanbekova/MediaWikiCrazyHackers.git
--- src/test/resources/com/MediaWiki/features/CreateAccount.feature
+++ src/test/resources/com/MediaWiki/features/CreateAccount.feature
@@ -1,7 +1,9 @@
+  @account
 Feature: Create Account
 
   Background: 
     Given I open a MediaWiki page
+
 
   Scenario Outline: Verify error message with invalid registration data
     When I click on Create Account link
@@ -10,12 +12,10 @@ Feature: Create Account
 
     Examples: 
       | username | password | confirm_password | message                                                                            |
-      | Dana     | abc123   | abc123           | Username entered already in use.Please choose a different name                     |
+      | Dana     |          |                  | Username entered already in use.Please choose a different name                     |
       | Flower   | Flower   |                  | Your password must be different from your username                                 |
       | Flower   | abc123   | abc1234          | There are problems with some of your input. The passwords you entered do not match |
-      
-      
-@account
+
   Scenario: Create account using  Main page
     When I create a new account with random username
     Then I should be able to create a new account
